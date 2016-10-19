448471eef4e79c2a249afb316060069176e8c56a
Kouboura <koubouramama@Koubouras-MacBook-Air.local>
2016-10-18 22:06:41 -0400

    Redefinition de la classe Etudiant

--- lib/gestion-de-notes/Etudiant.rb
+++ lib/gestion-de-notes/Etudiant.rb
@@ -1,71 +1,45 @@
+require 'csv'
+
 module GestionDeNotes
 
 class Etudiant
 	attr_reader :code
 	attr_reader :nom
 	attr_reader :prenom
-	attr_accessor :projet1
-	attr_accessor :projet2
-	attr_accessor :examen
+	attr_accessor :notes
 	attr_accessor :moyenne
-	attr_accessor :rang
 	attr_accessor :cote
-end
-	etudiant = Array.new
 
-	def initialize(code, nom, prenom, projet1, projet2, examen, moyenne, rang, cote)
+	def initialize (code, nom, prenom)
 		@code = code
 		@nom = nom
-		@prenom = prenom
-		@projet1 = projet1
-		@projet2 = projet2
-		@examen = examen
-		@moyenne = moyenne
-		@rang = rang
-		@cote = cote
+		@prenom = prenom 
+		@notes = {}
+		@moyenne = 0
+		@cote = 0
 	end
 
+#Lecture du fichier CSV
+	def lecture_csv(fichier)
+	etudiants = Array.new  
+	CSV.foreach(fichier) do |ligne|
+	e = Etudiant.new(ligne[0], ligne[1], ligne[2])
+	etudiants.push(e)
+	puts e.nom
+	end
+	end	
 
-	def lecture_fichier1
-		#definir un tableau pour contenir les informations de chaque etudiants
-
-		
-
-		#ouvrir le fichier csv
-		CSV.foreach("BD_simplifiee-liste_etudiants.csv",{:headers => :first_row}) do |row|
-			fields = row.split(',')
-			e = Etudiant.new
-			e.code = fields[0]
-			e.nom = fields[1]
-			e.prenom = fields[2]
-			e.projet1 = fields[3]
-			e.projet2 = fields[4]
-			e.examen = fields[5]
-			e.moyenne = fields[6]
-			e.rang = fields[7]
-			e.cote = fields[8]
-			
-		etudiant.push(e)
+#Fonctions pour enregistrer une note 
+	def enregistrer_notes (code, note)
 		end
-		
-	end
 
-	def self.enregistrer_note(code,idDevoir,note)
-	#code = code permanent de l etudiant 
-	#idDevoir = code du devoir
-	#note = note de l etudiant
-		etudiant = Array.new
-		etudiant.each do |x|
-			if x[0][j] === code do
-				x[3][j] = note if x[3][0] === idDevoir
-				x[4][j] = note if x[4][0] === idDevoir
-				x[5][j] = note if x[5][0] === idDevoir
-			end
-		end
-	t = "enregistre"
-	return t
+	def afficher 
+		puts "code : " + @code.to_s + "Nom : " + @nom.to_s + "\nPrenom : " + @prenom.to_s + "\nNotes : " + @notes.to_s + "\nMoyenne :" + @moyenne.to_s + " \nCote : " + @cote.to_s + "\nRang centile : " + @rang_centile.to_s
 	end
-
-end
 end
 
+
+a = Etudiant.new(22, "MAMA", "president")
+a.lecture_csv('liste_etudiants_defaut.csv')
+# a.afficher
+end
