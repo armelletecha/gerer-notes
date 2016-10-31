cc622679c426a8c38c6d89a8b8aeb3b238eb6c44
armelletecha <armelle.tenekeu-chibemen.1@ens.etsmtl.ca>
2016-10-31 04:48:54 -0400

    methode enregistrer_note

--- lib/gestion-de-notes/etudiant.rb
+++ lib/gestion-de-notes/etudiant.rb
@@ -17,26 +17,8 @@ class Etudiant
 		@cote = 0
 		
 	end
-
-	
-	def self.enregistrer_note(code,id_devoir,note,tableau_etudiants,tableau_travaux)
-
-#verifier que l id_devoir specifie existe
-		tableau_travaux.each do |travail|
-			existe_travail = true if travail.id === id_devoir
-			existe_travail = false
-		end
-
-#inserer la note dans le tableau
-		tableau_etudiants.each do |etudiant|
-			etudiant.note[:id_devoir] = note if etudiant.code === code
-			return " note enregistree "
-		end
-	end
-
 end
 end
-
 
 
 
